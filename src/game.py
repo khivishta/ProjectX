d75d5de7e97481c445be699a3a1b3a0725f73a96
Khivishta <khivishta.boodhoo@nottingham.ac.uk>
2020-12-04 22:28:38 +0000

    collision all working

--- src/game.py
+++ src/game.py
@@ -3,7 +3,9 @@ import arcade
 from block import Block
 from mask import Mask
 from player import Player
-
+import pygame
+import time
+import sys
 # defining variable
 
 WINDOW_SIZE = (800, 600)
@@ -32,6 +34,7 @@ class Game:
         self.all_sprites = pygame.sprite.Group()
         self.all_masks = pygame.sprite.Group()
         self.all_blocks = pygame.sprite.Group()
+        self.all_players = pygame.sprite.Group()
         self.player = Player()
         self.score = 0
 
@@ -46,7 +49,7 @@ class Game:
                     y = height * i
                     block = Block(width, height, x, y)
                     self.all_sprites.add(block)
-                    self.all_masks.add(block)
+                    self.all_blocks.add(block)
 
         for i in range(N_MASKS):
             mask = Mask(WINDOW_SIZE)
@@ -57,37 +60,54 @@ class Game:
             self.all_masks.add(mask)
         self.done = False
         pygame.display.set_caption("Mask Warriors")
-        grey = (128, 128, 128)
-        self.screen.fill(grey)
-        self.all_sprites.add(self.player)
+        self.all_players.add(self.player)
 
 
         # draw the background
 
     def run(self):
         while not self.done:
-                       
+            pygame.display.flip()
+            grey = (128, 128, 128)
+            self.screen.fill(grey)
+            self.all_blocks.update()
+            self.all_masks.update()
+            self.all_players.update()
+            self.all_blocks.draw(self.screen)
+            self.all_masks.draw(self.screen)
+            self.all_players.draw(self.screen)
+            myfont = pygame.font.SysFont("monospace", 20)
+            scoretext = myfont.render("Score = "+str(self.score), 1, (255,0,0))
+            self.screen.blit(scoretext, (5, 10))
             self.clock.tick(60)
-            pygame.display.update()
 
-            # check if the player has intersected with any masks attempt.
-        
-            # if pygame.sprite.spritecollideany(self.player,self.all_masks,True):
-            #      self.score  =+ 10
-            #      print(self.score)
-            self.all_sprites.update()
-            self.all_sprites.draw(self.screen)
-
+            # check if the player has intersected with any masks attempt.                
+            if pygame.sprite.spritecollide(self.player, self.all_masks, True):                    
+                    self.score  += 10
+                    
+            while self.score == 10 * N_MASKS:
+                # they show winning screen
+                pygame.display.flip()
+                self.screen.fill((0,0,0))
+                myfont = pygame.font.SysFont("monospace", 50)
+                text = myfont.render("You won!", 1, (255,0,0))
+                text_rect = text.get_rect(center=(WINDOW_SIZE[0]/2, WINDOW_SIZE[1]/2))
+                self.screen.blit(text, text_rect)
+                pygame.display.update()
+                time.sleep(5)
+                pygame.quit()
+                sys.exit()
             #Cycle through all masks currently on screen attempt 2.
             # for self.mask in self.all_masks:
             #     mask_collided= arcade.check_for_collision_with_list(self.mask, self.player)
             #     for masks in mask_collided:
             #         self.mask.remove_from_sprite_lists()
 
-
+            pygame.display.update()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.done = True
+        
 
 
 game = Game()
